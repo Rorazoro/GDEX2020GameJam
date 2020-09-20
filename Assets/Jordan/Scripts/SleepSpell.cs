ab45a18b949adf94b565d939376d720afd679acd
Jordan Dube <32602604+JordanDube@users.noreply.github.com>
2020-09-20 14:02:25 -0400

    Sleep spell and EvilPanda Script

---
+++ Assets/Jordan/Scripts/SleepSpell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SleepSpell : MonoBehaviour//, IReceiveCast
+{
+    [SerializeField] private string mySpellPattern;
+
+    private bool isSpellActive = false;
+    void CastSpell(string pattern) //Change so it uses IReceiveCast
+    {
+        if (mySpellPattern == pattern)
+        {
+            isSpellActive = true;
+            StartCoroutine(Sleeping());
+        }
+    }
+
+    IEnumerator Sleeping()
+    {
+        gameObject.GetComponent<EvilPanda>().Sleep();
+        yield return new WaitForSeconds(5f);
+        gameObject.GetComponent<EvilPanda>().WakeUp();
+    }
+    
+}
