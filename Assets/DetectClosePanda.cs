c95c1e014d13fd88682fcfbd86d0f983c53028dd
Mara <m.alsadoon90@gmail.com>
2020-09-20 21:55:30 -0400

    Feature/detect-close-panda

---
+++ Assets/DetectClosePanda.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectClosePanda : MonoBehaviour {
+
+    void Update () {
+        
+        DetectClosestPanda ();
+    }
+
+    void DetectClosestPanda () {
+        float distanceToClosestPanda = Mathf.Infinity;
+        Panda closest = null;
+
+        Panda[] allPandas = GameObject.FindObjectsOfType<Panda> ();
+
+        foreach (Panda currentPanda in allPandas) {
+            float distanceToPanda = (currentPanda.transform.position - this.transform.position).sqrMagnitude;
+            if (distanceToPanda < distanceToClosestPanda) {
+                distanceToClosestPanda = distanceToPanda;
+                closest = currentPanda;
+            }
+
+        }
+        Debug.DrawLine (this.transform.position, closest.transform.position);
+
+    }
+}
