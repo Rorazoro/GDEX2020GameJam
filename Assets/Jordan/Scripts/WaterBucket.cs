cd64576e4603bd796c9566d8535442d018d3e5a6
Jordan Dube <32602604+JordanDube@users.noreply.github.com>
2020-09-30 19:58:28 -0400

    magnetism and some terrain stuff

--- Assets/Jordan/Scripts/WaterBucket.cs
+++ Assets/Jordan/Scripts/WaterBucket.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Configuration;
 using UnityEngine;
 
 public class WaterBucket : MonoBehaviour
 {
     [SerializeField] private GameObject innerWater;
+    public bool isFull = false;
     private void OnTriggerEnter(Collider other)
     {
+        print("I hit " + other.gameObject.tag);
         if (other.gameObject.tag == "Water")
         {
-            gameObject.tag = "FullBucket";
+            print("got water");
+            other.gameObject.tag = "FullBucket";
             innerWater.SetActive(true);
+            isFull = true;
         }
 
         if (other.gameObject.tag == "Fire")
         {
             gameObject.tag = "Untagged";
             innerWater.SetActive(false);
+            isFull = false;
         }
     }
 }
