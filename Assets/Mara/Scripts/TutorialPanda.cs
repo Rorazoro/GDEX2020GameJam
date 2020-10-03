c9216cba551d8417af56cc034fc831068b124576
Mara <m.alsadoon90@gmail.com>
2020-10-03 01:20:05 -0400

    added the tutorial Panda Finding spell
    just a bamboo pole that gives some tutorial, when you click on it, you have to write the letter "P" to find your first panda

---
+++ Assets/Mara/Scripts/TutorialPanda.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialPanda : MonoBehaviour {
+    public GameObject player;
+    public GameObject tutorialCanvas;
+    void OnTriggerEnter (Collider col) {
+        if (col.name == player.name) {
+            tutorialCanvas.SetActive (true);
+        }
+    }
+
+    void OnTriggerExit (Collider col) {
+        if (col.name == player.name) {
+            tutorialCanvas.SetActive (false);
+        }
+    }
+}
